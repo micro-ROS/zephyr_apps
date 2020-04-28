61874f1d9aa3d0253180e3b782861175ffee0ebb
Pablo Garrido <pablogs9@gmail.com>
2020-04-28 08:04:20 +0200

    Style

--- microros_extensions/zephyr_usb_serial_transport.c
+++ microros_extensions/zephyr_usb_serial_transport.c
@@ -19,44 +19,42 @@ u8_t uart_out_buffer[RING_BUF_SIZE];
 
 struct ring_buf out_ringbuf, in_ringbuf;
 
-static void uart_fifo_callback(struct device *dev)
-{ 
-	while (uart_irq_update(dev) && uart_irq_is_pending(dev)) {
-		if (uart_irq_rx_ready(dev)) {
-			int recv_len;
-			u8_t buffer[64];
-			size_t len = MIN(ring_buf_space_get(&in_ringbuf), sizeof(buffer));
+static void uart_fifo_callback(struct device *dev){ 
+		while (uart_irq_update(dev) && uart_irq_is_pending(dev)) {
+			if (uart_irq_rx_ready(dev)) {
+				int recv_len;
+				u8_t buffer[64];
+				size_t len = MIN(ring_buf_space_get(&in_ringbuf), sizeof(buffer));
 
-      if (len > 0){
-        recv_len = uart_fifo_read(dev, buffer, len);
-			  ring_buf_put(&in_ringbuf, buffer, recv_len);
-      }
-      
-		}
+				if (len > 0){
+					recv_len = uart_fifo_read(dev, buffer, len);
+					ring_buf_put(&in_ringbuf, buffer, recv_len);
+				}
 
-		if (uart_irq_tx_ready(dev)) {			
-			u8_t buffer[64];
-			int rb_len;
-
-			rb_len = ring_buf_get(&out_ringbuf, buffer, sizeof(buffer));
-
-			if (rb_len == 0) {
-				uart_irq_tx_disable(dev);
-				continue;
 			}
 
-			uart_fifo_fill(dev, buffer, rb_len);
+			if (uart_irq_tx_ready(dev)) {			
+				u8_t buffer[64];
+				int rb_len;
+
+				rb_len = ring_buf_get(&out_ringbuf, buffer, sizeof(buffer));
+
+				if (rb_len == 0) {
+					uart_irq_tx_disable(dev);
+					continue;
+				}
+
+				uart_fifo_fill(dev, buffer, rb_len);
+			}
 		}
-	}
 }
 
 
-bool uxr_init_serial_platform(struct uxrSerialPlatform* platform, int fd, uint8_t remote_addr, uint8_t local_addr)
-{  
-  int ret;
-  u32_t baudrate, dtr = 0U;
- 
- platform->uart_dev = device_get_binding("CDC_ACM_0");
+bool uxr_init_serial_platform(struct uxrSerialPlatform* platform, int fd, uint8_t remote_addr, uint8_t local_addr){  
+	int ret;
+	u32_t baudrate, dtr = 0U;
+
+	platform->uart_dev = device_get_binding("CDC_ACM_0");
 	if (!platform->uart_dev) {
 		printk("CDC ACM device not found\n");
 		return false;
@@ -68,7 +66,7 @@ bool uxr_init_serial_platform(struct uxrSerialPlatform* platform, int fd, uint8_
 		return false;
 	}
 
-  ring_buf_init(&out_ringbuf, sizeof(uart_out_buffer), uart_out_buffer);
+	ring_buf_init(&out_ringbuf, sizeof(uart_out_buffer), uart_out_buffer);
 	ring_buf_init(&in_ringbuf, sizeof(uart_in_buffer), uart_out_buffer);
 
 	printk("Waiting for agent connection\n");
@@ -108,38 +106,38 @@ bool uxr_init_serial_platform(struct uxrSerialPlatform* platform, int fd, uint8_
 
 	/* Enable rx interrupts */
 	uart_irq_rx_enable(platform->uart_dev);
-  
-  return true;
+
+	return true;
 }
 
-bool uxr_close_serial_platform(struct uxrSerialPlatform* platform)
-{   
-  return true;
+bool uxr_close_serial_platform(struct uxrSerialPlatform* platform){   
+  	return true;
 }
 
-size_t uxr_write_serial_data_platform(uxrSerialPlatform* platform, uint8_t* buf, size_t len, uint8_t* errcode)
-{ 
-  size_t wrote;
-  
-  wrote = ring_buf_put(&out_ringbuf, buf, len);
-  
-  uart_irq_tx_enable(platform->uart_dev);
+size_t uxr_write_serial_data_platform(uxrSerialPlatform* platform, uint8_t* buf, size_t len, uint8_t* errcode){ 
+	size_t wrote;
+	
+	wrote = ring_buf_put(&out_ringbuf, buf, len);
+	
+	uart_irq_tx_enable(platform->uart_dev);
 
-  while (!ring_buf_is_empty(&out_ringbuf)){
-    k_sleep(K_MSEC(5));
-  }
-  
-  return wrote;
+	while (!ring_buf_is_empty(&out_ringbuf)){
+		k_sleep(K_MSEC(5));
+	}
+	
+	return wrote;
 }
 
-size_t uxr_read_serial_data_platform(uxrSerialPlatform* platform, uint8_t* buf, size_t len, int timeout, uint8_t* errcode)
-{ 
-	// int acc_timeout = 0;
-	// size_t read;
+size_t uxr_read_serial_data_platform(uxrSerialPlatform* platform, uint8_t* buf, size_t len, int timeout, uint8_t* errcode){ 
 
-	// uart_irq_rx_disable(platform->uart_dev);
-	// read = ring_buf_get(&in_ringbuf, buf, len);
-	// uart_irq_rx_enable(platform->uart_dev);
+	k_sleep(K_MSEC(timeout));
+
+	uart_irq_rx_disable(platform->uart_dev);
+
+	size_t read;
+	read = ring_buf_get(&in_ringbuf, buf, len);
+
+	uart_irq_rx_enable(platform->uart_dev);
 
 	// if(read < len){
 	// 	k_sleep(K_MSEC(timeout));
@@ -149,16 +147,5 @@ size_t uxr_read_serial_data_platform(uxrSerialPlatform* platform, uint8_t* buf, 
 	// 	uart_irq_rx_enable(platform->uart_dev);
 	// }
 
-	// return read;
-
-  k_sleep(K_MSEC(timeout));
-
-  uart_irq_rx_disable(platform->uart_dev);
-
-  size_t read;
-  read = ring_buf_get(&in_ringbuf, buf, len);
-
-  uart_irq_rx_enable(platform->uart_dev);
-
-  return read;
+  	return read;
  }
