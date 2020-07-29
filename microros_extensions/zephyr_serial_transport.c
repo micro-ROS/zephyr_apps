560ca931d5468a3b7fa737b0ce67e0506e1dca55
Pablo Garrido <pablogs9@gmail.com>
2020-07-29 07:45:51 +0200

    Fix fifo callback duplication

--- microros_extensions/zephyr_serial_transport.c
+++ microros_extensions/zephyr_serial_transport.c
@@ -35,25 +35,6 @@ static void uart_fifo_callback(struct device *dev){
     }
 }
 
-struct ring_buf out_ringbuf, in_ringbuf;
-
-static void uart_fifo_callback(struct device *dev){ 
-    while (uart_irq_update(dev) && uart_irq_is_pending(dev)) {
-        if (uart_irq_rx_ready(dev)) {
-            int recv_len;
-            u8_t buffer[64];
-            size_t len = MIN(ring_buf_space_get(&in_ringbuf), sizeof(buffer));
-
-            if (len > 0){
-                recv_len = uart_fifo_read(dev, buffer, len);
-                ring_buf_put(&in_ringbuf, buffer, recv_len);
-            }
-        }else{
-            break;
-        }
-    }
-}
-
 bool uxr_init_serial_platform(struct uxrSerialPlatform* platform, int fd, uint8_t remote_addr, uint8_t local_addr){  
 
     char uart_descriptor[8]; 
